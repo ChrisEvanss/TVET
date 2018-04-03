bc782c988e84a33afa6dd43e93e988c20598b007
chrisevanss <cooldude55@live.com.au>
2018-04-03 15:57:23 +1000

    Worked on GoneHome Add new script called "Player"

---
+++ Assets/~GoneHome/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Clean Code: CTRL + K + D
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody rigid;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}

79c123d894b3e060b81634651ef1cd52b9133bcc
ChrisEvanss <cooldude55@live.com.au>
2018-11-13 15:37:07 +1100

    Made moving objects and GameManager

---
+++ Arma_Sem2018/Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> tiles = new List<GameObject>();
+
+    float timerTotal;
+    float timerCurrent;
+
+    public int index;
+
+    // Use this for initialization
+    void Start()
+    {
+        timerTotal = 3;
+        timerCurrent = timerTotal;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timerCurrent = Time.deltaTime;
+        if (timerCurrent <= 0)
+        {
+            Instantiate(tiles[index], new Vector3(0, 5, 0), Quaternion.identity);
+            timerCurrent = timerTotal;
+            if (index < tiles.Count)
+            {
+                index++;
+            }
+        }
+
+    }
+}
